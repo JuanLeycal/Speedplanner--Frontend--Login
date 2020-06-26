849590bbf04b9d6e3328998621c51f239380d694
empeleycal <leyva.calle1403gmail.com>
2020-06-26 12:17:27 -0500

    Creación de wireframes de Login
    Html y css usado, con sus rutas incluidas

--- src/components/FindPassword.vue
+++ src/components/FindPassword.vue
@@ -1,6 +1,40 @@
 <template>
     <v-card>
-        <a>prueba</a>
+        <v-row>
+          <v-col class="space1"  cols="12" sm="12"></v-col>
+          <v-col class="space2"  cols="12" sm="12"></v-col>
+            <v-col class = "imga" cols="12" sm="12">
+                <div class="container" style="clear: right">
+                        <v-img class="imag" position="25% 75%" src = @/assets/speedplanner.png aspect-ratio = "7.5">
+                        </v-img>
+                </div>
+            </v-col>
+        </v-row>
+        <v-col class="blue"  cols="12" sm="12"></v-col>
+        <v-card-text class = "white--text">
+        <v-form v-model="isValid" class = "Inicio" >
+            <v-card-text class ="text" cols="12" sm="12">¿Olvidaste la contraseña? Tranquilo y recuerda</v-card-text>
+            <p class = "correo">Correo</p>
+            <v-text-field single-line rounded filled outlined color="white" dark
+                          v-model="remember"
+                          :rules="[v => !!v || 'Ingresar Correo']"
+                          required>
+            </v-text-field>
+            <v-col class="text-center">
+                <v-btn href="/passwordFound" outlined color="white" rounded class="Accept">Recuperar</v-btn>
+            </v-col>
+        </v-form>
+        </v-card-text >
+        <v-col class="blue"  cols="12" sm="12"></v-col>
+        <div class="default">
+            <v-col class="default"  cols="12" sm="12"></v-col>
+        </div>
+        <v-row>
+          <v-col class="space2"  cols="12" sm="12"></v-col>
+          <v-col class="space3"  cols="12" sm="12">
+            <v-card-text class ="Domain">Adaptimize, 2019</v-card-text>
+          </v-col>
+        </v-row>
     </v-card>
 </template>
 
@@ -9,7 +43,7 @@
     export default {
         name: "FindPassword",
         data: () => ({
-                email: null,
+                remember: null,
 
             }
         ),
@@ -25,5 +59,65 @@
 </script>
 
 <style scoped>
+    .blue{
+        background-color: #3B62B7;
+    }
+    .default{
+        background-color: #2E3D4E ;
+        height: 100px;
+    }
+    .imga{
+        background-color: #2E3D4E;
+
+    }
+    .space1{
+        background-color: #3055AB;
+        height: 70px;
+    }
+    .space2{
+        background-color: #D3D6E2;
+        height: 5px;
+    }
+    .space3{
+        background-color: #9F3833;
+        height: 50px;
+        font-weight: bold;
+        font-family: 'Khula', sans-serif;
+        color: #FFFFFF !important;
+        text-align: right;
+    }
+    .correo{
+        font-size: 2em;
+    }
+    .text{
+        font-size: 3em;
+        font-weight: bold;
+        font-style: italic;
+        color: #FFFFFF !important;
+        text-align: center;
+    }
+    .white--text{
+        background-color: #23313F;
+        color: #FFFFFF !important;
+        font-family: 'Khula', sans-serif;
+    }
+    .imag{
+        opacity: 0.5;
+    }
+    .Accept{
+        width: 150px;
+        height: 40px;
+        font-size: 18px;
+        color:white;
+        font-family: 'Khula', sans-serif;
+        font-style: normal;
+        border-radius: 20px;
+        border-style: solid;
+        border-color: #FFFFFF;
+        background-color: #3457A1;
+        border-width: thick;
+        margin-left: 656px;
+
+    }
 
 </style>
--- src/components/PasswordFound.vue
+++ src/components/PasswordFound.vue
@@ -1,5 +1,38 @@
 <template>
-    <h1>prueba</h1>
+    <v-card>
+        <v-row>
+            <v-col class="space1"  cols="12" sm="12"></v-col>
+            <v-col class="space2"  cols="12" sm="12"></v-col>
+            <v-col class = "imga" cols="12" sm="12">
+                <div class="container" style="clear: right">
+                    <v-img class="imag" position="25% 75%" src = @/assets/speedplanner.png aspect-ratio = "7.5">
+                    </v-img>
+                </div>
+            </v-col>
+        </v-row>
+        <v-col class="blue"  cols="12" sm="12"></v-col>
+        <v-card-text class = "white--text">
+            <v-form v-model="isValid" class = "Inicio" >
+                <v-card-text class ="text" cols="12" sm="12">Se detectó una cuenta. Se envió un correo con la contraseña</v-card-text>
+
+                    <div class="text-center">
+                    <v-btn  outlined color="white" rounded class="Log">No recibi un correo</v-btn>
+                    <v-btn href="/" outlined color="white" rounded class="Accept">Volver al Login</v-btn>
+                    </div>
+
+            </v-form>
+        </v-card-text >
+        <v-col class="blue"  cols="12" sm="12"></v-col>
+        <div class="default">
+            <v-col class="default"  cols="12" sm="12"></v-col>
+        </div>
+        <v-row>
+            <v-col class="space2"  cols="12" sm="12"></v-col>
+            <v-col class="space3"  cols="12" sm="12">
+                <v-card-text class ="Domain">Adaptimize, 2019</v-card-text>
+            </v-col>
+        </v-row>
+    </v-card>
 </template>
 
 <script>
@@ -9,5 +42,78 @@
 </script>
 
 <style scoped>
+    .blue{
+        background-color: #3B62B7;
+    }
+    .default{
+        background-color: #2E3D4E ;
+        height: 100px;
+    }
+    .imga{
+        background-color: #2E3D4E;
 
+    }
+    .space1{
+        background-color: #3055AB;
+        height: 70px;
+    }
+    .space2{
+        background-color: #D3D6E2;
+        height: 5px;
+    }
+    .space3{
+        background-color: #9F3833;
+        height: 50px;
+        font-weight: bold;
+        font-family: 'Khula', sans-serif;
+        color: #FFFFFF !important;
+        text-align: right;
+    }
+    .correo{
+        font-size: 2em;
+    }
+    .text{
+        font-size: 3em;
+        font-weight: bold;
+        font-style: italic;
+        color: #FFFFFF !important;
+        text-align: center;
+    }
+    .white--text{
+        background-color: #23313F;
+        color: #FFFFFF !important;
+        font-family: 'Khula', sans-serif;
+    }
+    .imag{
+        opacity: 0.5;
+    }
+    .Accept{
+        width: 200px;
+        height: 60px;
+        font-size: 18px;
+        color:white;
+        font-family: 'Khula', sans-serif;
+        font-style: normal;
+        border-radius: 20px;
+        border-style: solid;
+        border-color: #FFFFFF;
+        background-color: #3457A1;
+        border-width: thick;
+        margin-left: 280px;
+
+    }
+    .Log{
+        width: 250px;
+        height: 60px;
+        font-size: 18px;
+        color:white;
+        font-family: 'Khula', sans-serif;
+        font-style: normal;
+        border-radius: 20px;
+        border-style: solid;
+        border-color: #FFFFFF;
+        background-color: #A33D37;
+        border-width: thick;
+        margin-left: 400px;
+    }
 </style>
