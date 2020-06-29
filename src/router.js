d5c455a324c02675b565dbb4a09af558821e31cd
GinoQuispe21 <gino_21quispe@outlook.com>
2020-06-29 18:12:57 -0500

    Router bugs fixed

--- src/router.js
+++ src/router.js
@@ -9,18 +9,15 @@ export default new Router({
         { path: "/", alias: "/login", name: "login", component: () => import("./components/Login") },
         { path: "/findPassword", alias: "/findPassword", name: "findPassword", component: () => import("./components/FindPassword") },
         { path: "/passwordFound", alias: "/passwordFound", name: "foundPassword", component: () => import("./components/PasswordFound") },
-
         { path: "/adminLogin", alias: "/adminLogin", name: "adminLogin", component: () => import("./components/AdminLogin") },
         { path: "/noSectionRequestsFound", alias: "/noSectionRequestsFound", component: () => import("./components/NoSectionRequestsFound") },
         { path: "/sectionRequests", alias: "/sectionRequests", component: () => import("./components/SectionRequests") },
-
-        //{ path: "/adminLogin", alias: "/adminLogin", name: "adminLogin", component: () => import("./components/AdminLogin") },
-        //{ path: "/noSectionRequestsFound", alias: "/noSectionRequestsFound", component: () => import("./components/NoSectionRequestsFound") },
-        //{ path: "/sectionRequests", alias: "/sectionRequests", component: () => import("./components/SectionRequests") },
-
         { path: "/studentHome", alias: "/studentHome", name: "studentHome", component: () => import("./components/StudentHome")},
-        { path: "/cursoDeCarrera", alias: "/cursoDeCarrera", name: "cursoDeCarrera", component: () => import("./components/CursoDeCarrera")},
-        { path: "/cursoElectivo", alias: "/cursoElectivo", name: "cursoElectivo", component: () => import("./components/CursoElectivo")},
+        { path: "/careerCourse", alias: "/careerCourse", name: "careerCourse", component: () => import("./components/CareerCourse")},
+        { path: "/optionalCourse", alias: "/optionalCourse", name: "optionalCourse", component: () => import("./components/OptionalCourse")},
         { path: "/showSchedule", alias: "/showSchedule", name: "showSchedule", component: () => import("./components/ShowSchedule") },
+        { path: "/dialogSaveSchedule", alias: "/dialogSaveSchedule", name: "dialogSaveSchedule", component: () => import("./components/DialogSaveSchedule") },
+        { path: "/questionSaveSchedule", alias: "/questionSaveSchedule", name: "questionSaveSchedule", component: () => import("./components/QuestionSaveSchedule") },
+        { path: "/successfullyRecordSchedule", alias: "/successfullyRecordSchedule", name: "successfullyRecordSchedule", component: () => import("./components/SuccessfullyRecordSchedule") }
     ]
 });
